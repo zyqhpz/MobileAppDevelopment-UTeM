2b144739cabdc16514a5f086d5d368c4ac7c86ef
zyqq <zyqq.dev@gmail.com>
2022-10-14 09:03:50 +0800

    first commit

---
+++ Lab 1/app/src/main/java/com/example/mad_1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mad_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnHello = (Button) findViewById(R.id.btnHello);
+        btnHello.setText(getString(R.string.btnHello));
+    }
+}
