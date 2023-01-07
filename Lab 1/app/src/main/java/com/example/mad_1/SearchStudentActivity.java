08e679a1e5e79f7b52115bf5d3de3ffa713f3fac
Haziq Hapiz <haziq.hapiz@gmail.com>
2023-01-07 12:35:29 +0800

    add searchstudentactivity

---
+++ Lab 1/app/src/main/java/com/example/mad_1/SearchStudentActivity.java
@@ -0,0 +1,14 @@
+package com.example.mad_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_student);
+    }
+}
